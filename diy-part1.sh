b774b17d4c8c5dde151309b85af591e3787d8f48
jianping0754 <39966195+jianping0754@users.noreply.github.com>
2021-08-12 20:27:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,4 +22,4 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-th
 git clone https://github.com/jianping0754/luci-theme-darkmatter.git package/lean/luci-theme-darkmatter
 git clone https://github.com/kiddin9/luci-theme-edge.git package/lean/luci-theme-edge
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
-git clone https://github.com/github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
