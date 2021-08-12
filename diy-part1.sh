3003c110938adc21f78d5b117dacbd552dd1f6ba
jianping0754 <39966195+jianping0754@users.noreply.github.com>
2021-08-12 19:06:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,11 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git onliner https://github.com/rufengsuixing/luci-app-onliner' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git poweroff https://github.com/esirplayground/luci-app-poweroff' >>feeds.conf.default
+echo 'src-git theme-argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+echo 'src-git theme-darkmatter https://github.com/jianping0754/luci-theme-darkmatter' >>feeds.conf.default
+echo 'src-git theme-edge https://github.com/kiddin9/luci-theme-edge' >>feeds.conf.default
+echo 'src-git theme-infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' >>feeds.conf.default
+echo 'src-git theme-rosy https://github.com/rosywrt/luci-theme-rosy' >>feeds.conf.default
