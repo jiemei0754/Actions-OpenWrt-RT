51955fbbd78cfbeb47bb8cb8a24e5fd0e53d69d8
jianping0754 <39966195+jianping0754@users.noreply.github.com>
2021-08-12 19:28:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,11 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 添加插件源码
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git onliner https://github.com/rufengsuixing/luci-app-onliner' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git poweroff https://github.com/esirplayground/luci-app-poweroff' >>feeds.conf.default
-echo 'src-git theme-argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
-echo 'src-git theme-darkmatter https://github.com/jianping0754/luci-theme-darkmatter' >>feeds.conf.default
-echo 'src-git theme-edge https://github.com/kiddin9/luci-theme-edge' >>feeds.conf.default
-echo 'src-git theme-infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' >>feeds.conf.default
-echo 'src-git theme-rosy https://github.com/rosywrt/luci-theme-rosy' >>feeds.conf.default
+#echo 'src-git onliner https://github.com/rufengsuixing/luci-app-onliner' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git poweroff https://github.com/esirplayground/luci-app-poweroff' >>feeds.conf.default
+#echo 'src-git theme-argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
+#echo 'src-git theme-darkmatter https://github.com/jianping0754/luci-theme-darkmatter' >>feeds.conf.default
+#echo 'src-git theme-edge https://github.com/kiddin9/luci-theme-edge' >>feeds.conf.default
+#echo 'src-git theme-infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' >>feeds.conf.default
+#echo 'src-git theme-rosy https://github.com/rosywrt/luci-theme-rosy' >>feeds.conf.default
