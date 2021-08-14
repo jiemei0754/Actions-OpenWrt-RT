782a796d9c2414228a6b6a5be2b2fc5ec516b2cc
jianping0754 <39966195+jianping0754@users.noreply.github.com>
2021-08-14 20:30:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,14 +10,25 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 更新feeds文件
+# sed -i 's@#src-git helloworld@src-git helloworld@g' feeds.conf.default #启用helloworld
+cat feeds.conf.default
+
+# 添加第三方软件包
+git clone https://github.com/281677160/openwrt-package.git -b 18.06 package/dbone-packages
+
+# 更新并安装源
+./scripts/feeds clean
+./scripts/feeds update -a && ./scripts/feeds install -a
+
 # 去除源码注释
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 添加插件源码
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/john-shine/luci-theme-darkmatter.git package/lean/luci-theme-darkmatter
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
-git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#git clone https://github.com/john-shine/luci-theme-darkmatter.git package/lean/luci-theme-darkmatter
+#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
+#git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
