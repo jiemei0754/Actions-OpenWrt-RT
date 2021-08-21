7fbc037f5de27b67412e0aef534912618f090636
jiemei0754 <66130861+jiemei0754@users.noreply.github.com>
2021-08-21 14:19:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
-git clone https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
 git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
