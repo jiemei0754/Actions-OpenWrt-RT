fcba280df5f477fd3468c79fa178434cc4dc5a7f
jiemei0754 <66130861+jiemei0754@users.noreply.github.com>
2024-08-01 19:03:56 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
 #
 
 # 去除源码注释
@@ -15,9 +15,17 @@
 
 # 添加插件包
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/281677160/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
-git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/jianping0754/luci-app-poweroff.git package/lean/luci-app-poweroff
+git clone https://github.com/jianping0754/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jianping0754/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom
+git clone https://github.com/jianping0754/luci-theme-rosy.git package/lean/luci-theme-rosy
+# git clone https://github.com/jianping0754/OpenWrt-Theme/luci-theme-infinityfreedom.git package/theme
+
+# 移除 openwrt feeds 自带的核心包
+# rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+# git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
+
+# 更新 golang 1.22 版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
 #
 
 # 自定义定制选项
 ZZZ="package/lean/default-settings/files/zzz-default-settings"
 #
 sed -i "/uci commit fstab/a\uci commit network" $ZZZ
-sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.32.4'" $ZZZ                      # IPv4 地址(openwrt后台地址)
-sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ                    # IPv4 子网掩码
-sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.32.2'" $ZZZ                     # IPv4 网关
-sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.32.255'" $ZZZ                 # IPv4 广播
-sed -i "/uci commit network/i\uci set network.lan.dns='192.168.32.202 202.96.128.86'" $ZZZ         # DNS(多个DNS要用空格分开)
-sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                               # 去掉LAN口使用内置的 IPv6 管理
-echo "close_dhcp" > package/base-files/files/etc/closedhcp                                         # 关闭DHCP服务
+sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.32.4'" $ZZZ                                     # IPv4 地址(openwrt后台地址)
+sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ                                   # IPv4 子网掩码
+sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.32.2'" $ZZZ                                    # IPv4 网关
+sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.32.255'" $ZZZ                                # IPv4 广播
+sed -i "/uci commit network/i\uci set network.lan.dns='192.168.32.202 202.96.128.86 202.96.128.166'" $ZZZ         # DNS(多个DNS要用空格分开)
+sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                                              # 去掉LAN口使用内置的 IPv6 管理
+echo "close_dhcp" > package/base-files/files/etc/closedhcp                                                        # 关闭DHCP服务
 
-sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' feeds/luci/collections/luci/Makefile  # 选择infinityfreedom为默认主题
-sed -i "s/OpenWrt /Standard Edition Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ    # 增加个性名字Standard Edition
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-RT'" $ZZZ                 # 修改主机名称为OpenWrt-RT
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                             # 设置密码为空
-sed -i 's/PATCHVER:=5.15/PATCHVER:=6.1/g' target/linux/x86/Makefile                                # 修改内核为6.1
+sed -i 's/luci-theme-bootstrap/infinityfreedom/' feeds/luci/collections/luci/Makefile                             # 选择infinityfreedom为默认主题
+sed -i "s/OpenWrt /Standard Edition Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ                   # 增加个性化版本名Standard Edition
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-RT'" $ZZZ                                # 修改主机名称为OpenWrt-GT
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                                            # 设置密码为空
+sed -i 's/PATCHVER:=6.1/PATCHVER:=6.6/g' target/linux/x86/Makefile                                                # 修改内核为6.6
